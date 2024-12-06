0c788441bdc9cebe2e947ba1213253c98a48bf64
Vishnu hari <vishnu@hari.com>
2024-12-06 14:23:49 -0500

    Add project files.

---
+++ EventAndMediaHub/Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+namespace EventAndMediaHub.Models
+{
+    public class ErrorViewModel
+    {
+        internal List<string> Errors;
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
